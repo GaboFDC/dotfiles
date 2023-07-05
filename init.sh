a3fd187017010a0abb1b050acea253e22f78d6b1
Gabo <gabo.fdc@gmail.com>
2023-07-05 16:19:33 -0500

    Add steps for pyenv

--- init.sh
+++ init.sh
@@ -70,3 +70,11 @@ fi
 
 echo -e "\n\n\nInstalling vim plug..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo -e "\n\n\nInstalling pyenv..."
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+cd ~/.pyenv && src/configure && make -C src
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
